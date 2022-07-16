268be008f0db079ee2e1625cf393f14a0819f309
jainpranav1 <pranavji@tamu.edu>
2022-07-15 21:50:31 -0500

    added routes for navbar

--- index.js
+++ index.js
@@ -8,4 +8,32 @@ app.get("/", (req, res) => {
   res.render("home");
 });
 
+app.get("/about_me", (req, res) => {
+  res.render("about_me");
+});
+
+app.get("/academics", (req, res) => {
+  res.render("academics");
+});
+
+app.get("/work_experience", (req, res) => {
+  res.render("work_experience");
+});
+
+app.get("/leadership", (req, res) => {
+  res.render("leadership");
+});
+
+app.get("/projects", (req, res) => {
+  res.render("projects");
+});
+
+app.get("/achievements", (req, res) => {
+  res.render("achievements");
+});
+
+app.get("/volunteering", (req, res) => {
+  res.render("volunteering");
+});
+
 app.listen(5000);
